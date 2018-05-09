5b7e1e756c0d14c3b2c94356a44b0e32f42b34cd
Sule, Nishant V <nsule@seas.harvard.edu>
2018-05-09 15:55:12 -0400

    ask user for noise value

--- Vicsek2D.py
+++ Vicsek2D.py
@@ -12,9 +12,9 @@ class Vicsek2D:
         self.N = N  # number of particles in the 2D region
         self.rho = N/self.L**2  # density of particles in the simulation
         self.eta = eta  # noise in the system
-        self.r = r  # interaction radius
+        self.r = 1  # interaction radius
         self.dt = 1.0  # time step
-        self.v = v  # magnitude of velocity
+        self.v = 0.03  # magnitude of velocity
         self.pos = np.random.rand(self.N, 2) * self.L  # random initial position in 2D region
         self.theta = (np.random.rand(self.N) * 2 - 1) * np.pi  # random velocity angle [-pi pi]
         self.vel = np.zeros((self.N, 2))  # initialize velocity array
@@ -23,6 +23,8 @@ class Vicsek2D:
 
 
 # Ask user for parameters
-N = np.int(input("Enter the number of particles you want to simulate: "))
-v2d = Vicsek2D(N, 0.1, 1, 0.03)
+print("We will simulate a square box of size 25")
+N = int(input("Enter the number of particles you want in the box: "))
+eta = float(input("Enter the maximum noise in the system: "))
+v2d = Vicsek2D(N, eta)
 print("Particle density =", v2d.rho)
