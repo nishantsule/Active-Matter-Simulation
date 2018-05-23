2d1075488633bad0f5c00e6203a47148b1e0e23d
Sule, Nishant V <nsule@seas.harvard.edu>
2018-05-23 11:59:18 -0400

    completed update function

--- Vicsek2D.py
+++ Vicsek2D.py
@@ -9,10 +9,12 @@ class Vicsek2D:
     def __init__(self, N, eta):
         # Initialize simulation
         self.L = 25  # length of the square 2D region to be simulated
+        self.halfL = self.L / 2  # half of length (used later for PBCs)
         self.N = N  # number of particles in the 2D region
         self.rho = N/self.L**2  # density of particles in the simulation
         self.eta = eta  # noise in the system
         self.r = 1  # interaction radius
+        self.rsq = self.r * self.r  # square of interaction radius
         self.dt = 1.0  # time step
         self.v = 0.03  # magnitude of velocity
         self.pos = np.random.rand(self.N, 2) * self.L  # random initial position in 2D region
@@ -20,7 +22,8 @@ class Vicsek2D:
         self.vel = np.zeros((self.N, 2))  # initialize velocity array
         self.vel[:, 0] = self.v * np.cos(self.theta)  # velocity along x
         self.vel[:, 1] = self.v * np.sin(self.theta)  # velocity along y
-        self.tt = 20  # total number of time steps
+        self.tt = 50  # total number of time steps
+        self.rparts = np.eye(N, dtype=np.bool)  # matrix representing particles within distance r
 
     def main(self):
         # Setup plots
@@ -30,14 +33,68 @@ class Vicsek2D:
         fig.show()
         fig.tight_layout()
         fig.canvas.draw()
+        axrange = [0, self.L, 0, self.L]
         for nn in range(self.tt):
             ax.clear()
             ax.quiver(self.pos[:, 0], self.pos[:, 1], self.vel[:, 0], self.vel[:, 1])
+            ax.axis(axrange)
+            ax.set_aspect('equal', 'box')
             fig.canvas.draw()
             self.update()
 
     def update(self):
         # Update position and velocity
+
+        # Generate the set of random movements dTheta from [-eta/2, eta/2]
+        noise = (np.random.rand(self.N) - 0.5) * self.eta
+
+        # Find particles within distance r
+        self.find_particles()
+
+        # Initialize average theta
+        avg_theta = np.zeros(self.N)
+
+        for i in range(N):
+
+            # Angles of particles within separation r
+            rtheta = self.theta[self.rparts[i, :]]
+            avg_theta[i] = np.arctan2(np.mean(np.sin(rtheta)), np.mean(np.cos(rtheta)))
+
+        # Updated angles = avg. angles + noise
+        self.theta = avg_theta + noise
+
+        # Updated velocities
+        self.vel[:, 0] = self.v * np.cos(self.theta)
+        self.vel[:, 1] = self.v * np.sin(self.theta)
+
+        # Updated positions
+        self.pos = self.pos + self.vel * self.dt
+
+        # Applying periodic boundaries
+        self.pos = np.mod(self.pos, self.L)
+
+    def find_particles(self):
+
+        # Reset rparts matrix
+        self.rparts = np.eye(self.N, dtype=np.bool)
+
+        for i in range(N):
+            for j in range(i + 1, N):
+
+                diff = self.pos[i, :] - self.pos[j, :]
+
+                # Apply minimum image criteria for periodic boundaries on diff
+                for dim in range(2):
+                    while diff[dim] > self.halfL:
+                        diff[dim] = diff[dim] - self.L
+                    while diff[dim] < -self.halfL:
+                        diff[dim] = diff[dim] + self.L
+
+                # Separation between particles squared
+                sepsq = np.dot(diff, diff)
+                rflag = sepsq < self.rsq
+                self.rparts[i, j] = rflag
+                self.rparts[j, i] = rflag
 
 
 # Ask user for parameters
@@ -46,3 +103,4 @@ N = int(input("Enter the number of particles you want in the box: "))
 eta = float(input("Enter the maximum noise in the system: "))
 v2d = Vicsek2D(N, eta)
 print("Particle density =", v2d.rho)
+v2d.main()
